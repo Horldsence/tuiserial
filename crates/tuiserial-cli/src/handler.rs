f692b5e736c985746638f62ec23cbe3ef384f2af
Tanmay <nnon92333@gmail.com>
2026-06-16 15:22:30 +0530

    refactor: migrate serial port error handling to use thiserror crate

--- crates/tuiserial-cli/src/handler.rs
+++ crates/tuiserial-cli/src/handler.rs
@@ -1,7 +1,7 @@
 //! Serial port connection handler
 
 use tuiserial_core::AppState;
-use tuiserial_serial::serialport::SerialPort;
+use tuiserial_serial::{SerialError, serialport::SerialPort};
 
 /// Handler for managing serial port connections
 pub struct SerialHandler {
@@ -36,18 +36,18 @@ impl SerialHandler {
     }
 
     /// Send data to the serial port
-    pub fn send(&mut self, data: &[u8]) -> Result<usize, String> {
+    pub fn send(&mut self, data: &[u8]) -> Result<usize, SerialError> {
         match &mut self.port {
             Some(port) => tuiserial_serial::write_data(port.as_mut(), data),
-            None => Err("Port not connected".to_string()),
+            None => Err(SerialError::NotConnected),
         }
     }
 
     /// Read data from the serial port
-    pub fn read(&mut self) -> Result<Vec<u8>, String> {
+    pub fn read(&mut self) -> Result<Vec<u8>, SerialError> {
         match &mut self.port {
             Some(port) => tuiserial_serial::read_data(port.as_mut()),
-            None => Err("Port not connected".to_string()),
+            None => Err(SerialError::NotConnected),
         }
     }
 }
